a6190e65aacb0174921629040a22495cfbf2bae1
fadeaway1992 <498497303@qq.com>
2017-07-16 23:19:19 +0800

    保持光标在窗口中 / keep caret in the window

--- dist/js/more-editor.js
+++ dist/js/more-editor.js
@@ -1906,6 +1906,45 @@ function handleBackAndEnterKeydown(event) {
     }
 }
 
+/* 
+    每次 keydown 检查光标位置是否距离窗口底部距离太近，适当滚动文档，保持光标在窗口中。
+*/
+
+function checkCaretPosition (event) {
+    var node = MoreEditor.selection.getSelectionStart(this.options.ownerDocument)  
+
+    if (!node) {
+        return;
+    }
+
+    var selection = this.options.ownerDocument.getSelection()  
+    if(selection.rangeCount>0) {                               
+        var range = selection.getRangeAt(0)                    
+        var rects = range.getClientRects()                       // 获取选区／光标 clientRect 对象  对于光标换行，如果是从文本中间断句换行，可以获取到 rect 对象，如果是在文本末尾另起一行，这样是获取不到 rect 对象的。
+        var clineHeight = document.documentElement.clientHeight  // 获取当前客户端窗口高度
+        if(rects[0]) {
+            var bottom = clineHeight - rects[0].bottom           // 获取光标距离窗口底部的高度
+            if(bottom < 50) {
+                var scrollTop = this.options.ownerDocument.documentElement.scrollTop || this.options.ownerDocument.body.scrollTop  // 文档获取向上滚动的距离
+                this.options.ownerDocument.documentElement.scrollTop = scrollTop + rects[0].height    
+                this.options.ownerDocument.body.scrollTop = scrollTop + rects[0].height
+            }                                                                                  // 这一段是讲如果当前光标距离窗口底部很近了（<50），就将文档向上滚动一个光标的距离。
+        } else if (event.keyCode == 13) {    // 当前按下的键是 enter， 但是却没有获取到光标的 rect 对象。有些场景下无法获取到光标的 rect 对象，这时我们使用光标所在节点的父元素的 rect 对象。
+            var parentNode = MoreEditor.util.getClosestBlockContainer(node)  
+            if(!parentNode)return
+            var rect = parentNode.getBoundingClientRect()
+            var bottom = clineHeight - rect.bottom
+            if(bottom < 50) {
+                var scrollTop = this.options.ownerDocument.documentElement.scrollTop || this.options.ownerDocument.body.scrollTop
+                var height = rect.height + parseInt(getComputedStyle(parentNode).marginTop) + parseInt(getComputedStyle(parentNode).marginBottom)
+                this.options.ownerDocument.documentElement.scrollTop = scrollTop + height
+                this.options.ownerDocument.body.scrollTop = scrollTop + height    
+            }
+        }
+    }
+}
+
+
 /* MoreEditor 实例初始化时增添的一些属性 */
 var initialOptions = {
     contentWindow: window,
@@ -1919,6 +1958,7 @@ function initExtensions() {
 
 function attachHandlers() {
     this.on(this.editableElement, 'keydown', handleBackAndEnterKeydown.bind(this))
+    this.on(this.editableElement, 'keydown', checkCaretPosition.bind(this))
 }
 
 
