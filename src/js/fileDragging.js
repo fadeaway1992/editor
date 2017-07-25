a25b0de8453b99aa01e6728e61461e834ef4ce52
fadeaway1992 <498497303@qq.com>
2017-07-25 14:26:30 +0800

    拖拽图片

--- src/js/fileDragging.js
+++ src/js/fileDragging.js
@@ -5,6 +5,7 @@
 (function() {
   var lastTarget = null
   var line = null
+  var imageWrapperHTML = '<div data-type="more-editor-inserted-image" class="more-editor-inserted-image" contenteditable="false"><li data-type="image-placeholder" class="image-placeholder" contenteditable="true"></li></div>'
 
   var fileDragging = function(instance) {
     this.base = instance
@@ -16,8 +17,14 @@
     init: function() {
       line = document.createElement('div')
       line.classList.add('line')
+      document.addEventListener('dragover',function(event) {
+        event.preventDefault()
+      }.bind(this))
+      document.addEventListener('drop', function(event) {
+        event.preventDefault()
+      }.bind(this))
       this.base.editableElement.addEventListener('dragover', this.handleDrag.bind(this))
-      this.base.editableElement.addEventListener('dragenter', this.handleDragEnter.bind(this))
+      document.addEventListener('dragenter', this.handleDragEnter.bind(this))
       this.base.editableElement.addEventListener('drop', this.handleDrop.bind(this))
     },
 
@@ -27,42 +34,41 @@
     },
 
     handleDragEnter: function(event) {
-      console.log(event, 'event')
-      console.log(event.target, 'event.target')
+      if(!MoreEditor.util.isDescendant(this.base.editableElement, event.target, true)) {
+        if(line.parentNode) {
+          line.parentNode.removeChild(line)
+        }
+      }
       var target = MoreEditor.util.getTopBlockContainerWithoutMoreEditor(event.target)
       if(!target) return
       if(lastTarget != target) {
         MoreEditor.util.after(target, line)
-        console.log(line)
         lastTarget = target
       }
     },
 
     handleDrop: function(event) {
+      
       event.preventDefault()
       event.stopPropagation()
-      var target = MoreEditor.util.getTopBlockContainer(event.target)
-      MoreEditor.selection.selectNode(target, document)
-      var savedSelection = MoreEditor.selection.saveSelection(target)
-      savedSelection.start = savedSelection.end
-      MoreEditor.selection.restoreSelection(target,savedSelection)
+      
       if(event.dataTransfer.files[0].type.match('image')) {
-        this.insertImageFile(event.dataTransfer.files[0])
-      }
-    },
-
-    insertImageFile: function (file) {
-      if (typeof FileReader !== 'function') {
-          return;
-      }
-      var fileReader = new FileReader()
-      fileReader.readAsDataURL(file)
-
-      fileReader.addEventListener('load', function (e) {
+        var file = event.dataTransfer.files[0]
+        var fileReader = new FileReader()
+        fileReader.readAsDataURL(file)
+        fileReader.addEventListener('load', function (e) {
           var addImageElement = document.createElement('img')
+          addImageElement.classList.add('insert-image')
           addImageElement.src = e.target.result
-          document.execCommand('insertHTML', false, addImageElement.outerHTML);
-      }.bind(this))
+          var imageWrapper = document.createElement('div')
+          imageWrapper.innerHTML = imageWrapperHTML
+          var imagePlaceHolder = imageWrapper.querySelector('li')
+          MoreEditor.util.after(imagePlaceHolder, addImageElement)
+          MoreEditor.util.after(line, imageWrapper)
+          MoreEditor.util.unwrap(imageWrapper, document)
+          line.parentNode.removeChild(line)
+        }.bind(this))
+      }
     }
 
   }
