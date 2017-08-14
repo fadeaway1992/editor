b9b93b289d22a79d8d3508dc8bc28722c04316ab
fadeaway1992 <498497303@qq.com>
2017-08-14 13:49:24 +0800

    完善注释

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
+/* 
+    使用 gulp 进行 js 的合并、scss 的编译。
+    使用 browser-sync 进行热加载，实现多浏览器一同调试。
+*/
+
 var gulp = require('gulp')
 var autoprefixer = require('gulp-autoprefixer')
 var sass = require('gulp-sass')
 var concat = require('gulp-concat')
 var browserSync = require('browser-sync').create()
 
+
+/* 
+  将 /src 目录下的 JavaScript 文件按照下列顺序进行合并
+*/
 var concatArr = [
     'src/js/start.js',
     'src/js/globals.js',
@@ -20,16 +29,25 @@ var concatArr = [
     'src/js/end.js'
 ]
 
+/* 
+  注册 js 任务，按照指定顺序将 ／src 下的 JavaScript 文件合并为 /dist/js 下的 more-editor.js 文件
+*/
 gulp.task('js', function() {
     gulp.src(concatArr)
         .pipe(concat('more-editor.js'))
         .pipe(gulp.dest('dist/js'))
 })
 
+/* 
+  注册 js-watch 任务， 更新 more-editor.js 文件并刷新浏览器
+*/
 gulp.task('js-watch', ['js'], function() {
     browserSync.reload()
 })
 
+/* 
+  注册 css 任务，将 src/sass 下的所有 sass 文件转译为 css 文件放到 dist/css 下
+*/
 gulp.task('css', function() {
     gulp.src('src/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -40,7 +58,10 @@ gulp.task('css', function() {
     .pipe(browserSync.stream())
 })
 
-
+/* 
+  gulp 默认任务，在 /dist 下启动 browserSync 服务器，端口设置为 3000， 主页为 /dist/demo/index.html 
+  并监听 src/*.js src/sass/*.sass 实时更新。 
+*/
 gulp.task('default', ['js','css'], function() {
      
     browserSync.init({
